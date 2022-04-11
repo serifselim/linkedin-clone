d06fcd78d187938efa95a1fd85a5141de3c428ac
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-04-11 15:10:22 +0300

    stlye of register page was finished

--- src/imagePaths.js
+++ src/imagePaths.js
@@ -1,3 +1,4 @@
 export const IMAGE_URL = 'https://avatars.githubusercontent.com/u/70685127?v=4';
 export const BACKGROUND_URL = 'https://images.pexels.com/photos/11490754/pexels-photo-11490754.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260';
 export const FEED_IMAGE_URL = 'https://images.pexels.com/photos/11502643/pexels-photo-11502643.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
+export const LOGO_URL = 'https://marka-logo.com/wp-content/uploads/2020/04/Linkedin-Logo.png';
---
+++ src/pages/Register/Register.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { LoginContainer, LoginContent, Header, LoginActions } from '../Login/Login.styled';
+
+export const RegisterContainer = styled(LoginContainer)`
+`;
+
+export const RegisterContent = styled(LoginContent)``;
+
+export const CenterHeader = styled(Header)`
+    justify-content: center;
+`;
+
+export const RegisterActions = styled(LoginActions)`
+justify-content: center;
+max-width: 100%;
+width: 100%;
+    & > h1 {
+        text-align: center;
+        font-size: 3.6rem;
+        color: gray;
+    }
+`;
